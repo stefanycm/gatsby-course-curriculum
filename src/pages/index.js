48f65c55d8b81fe73511a4776f07d64c702088fd
Stefany Cárdenas Martínez <stefany.cardenas@pragma.com.co>
2022-05-02 10:26:21 -0500

    adding SASS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import './index.scss';
 
 const IndexPage = () => {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="title">Home</h1>
       <Button variant="primary">Click me</Button>
       <Button variant="danger">Danger</Button>
     </main>
